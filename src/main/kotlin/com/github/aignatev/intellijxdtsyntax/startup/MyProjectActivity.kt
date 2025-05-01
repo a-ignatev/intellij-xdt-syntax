abed54013411142f8b812dbe400c2bb84a3997f7
GitHub Action <action@github.com>
2025-05-01 09:14:55 +0000

    Template cleanup

--- src/main/kotlin/com/github/aignatev/intellijxdtsyntax/startup/MyProjectActivity.kt
+++ src/main/kotlin/com/github/aignatev/intellijxdtsyntax/startup/MyProjectActivity.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.startup
+package com.github.aignatev.intellijxdtsyntax.startup
 
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
